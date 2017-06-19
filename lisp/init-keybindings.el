1f0e2bb3043d5f0c8beb355895fc81b1646acbd8
zhangdaofeng <zhangdf0821@163.com>
2017-06-19 23:17:59 +0800

    C-p C-n to move in company-mode

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -4,6 +4,11 @@
 ;; ----------------------------------------------------
 (global-set-key (kbd "C-x C-r") 'recentf-open-files)
 (global-set-key (kbd "s-/") 'hippie-expand)
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
 
 
 ;; ----------------------------------------------------
